d784785c58b25f860fa4395a98b6688792f5a59b
Jaime <jaimehraraujo@gmail.com>
2026-06-10 14:49:30 -0300

    adicionados metodos de gravar endereço e telefone do usuario

--- src/main/java/com/javanauta/usuario/controller/UsuarioControler.java
+++ src/main/java/com/javanauta/usuario/controller/UsuarioControler.java
@@ -61,4 +61,14 @@ public class UsuarioControler {
                                                         @RequestParam("id") Long id){
         return ResponseEntity.ok(usuarioService.atualizaTelefone(id, dto));
     }
+    @PostMapping("/endereco")
+    public ResponseEntity<EnderecoDTO> cadastraEndereco(@RequestBody EnderecoDTO dto,
+                                                        @RequestHeader("Authorization") String token){
+        return ResponseEntity.ok(usuarioService.cadastraEndereco(token, dto));
+    }
+    @PostMapping("/telefone")
+    public ResponseEntity<TelefoneDTO> cadastraTelefone(@RequestBody TelefoneDTO dto,
+                                                        @RequestHeader("Authorization") String token){
+        return ResponseEntity.ok(usuarioService.cadastraTelefone(token, dto));
+    }
 }
